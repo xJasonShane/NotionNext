a839779d7083c7714165bc04db34f1925726e0c5
A-conanclub <jenson_shan@yeah.net>
2025-01-29 21:00:48 +0800

    更新配置，修改欢迎语为新的文本内容

--- blog.config.js
+++ blog.config.js
@@ -61,7 +61,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi,欢迎来到我的博客',
+    '年少轻狂 总以为天下事 无可不为, 岁月蹉跎 终感到天下人 力有尽头',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
